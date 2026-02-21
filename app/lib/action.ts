8da1c9ce0f591c98352ccb15040caf594158ce6e
spwine <101738450+spwine@users.noreply.github.com>
2026-02-21 15:20:30 +0700

    Update action.ts

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -31,20 +31,26 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
+// Update the signature to include prevState
+export async function updateInvoice(
+  id: string,
+  prevState: State, // This is the missing piece
+  formData: FormData,
+) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
   const amountInCents = amount * 100;
 
   try {
     await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
   } catch (error) {
     console.error(error);
     return {
